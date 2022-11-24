651809d2406678a0438f1cb2806ecc6ef14a69fd
Scott Jacobson <65079562+scojac-github@users.noreply.github.com>
2022-11-23 20:45:21 -0500

    Initial commit

---
+++ deleteTweets.R
@@ -0,0 +1,33 @@
+library(rtweet)
+library(tidyverse)
+library(ggplot2)
+
+# Auth into Twitter
+auth_setup_default()
+
+# Twitter username
+user <- "NotSJ215"
+
+# Get tweets
+tweets <- get_timeline(user, n = Inf)
+head(tweets, 5)
+
+# Save tweets for safekeeping
+write_csv(tweets, "data/tweets.csv")
+
+# Get your timeline
+my_timeline <- get_timeline(user)
+head(my_timeline, 5)
+colnames(my_timeline)
+
+# Create a list of all tweets
+tweet_ids <- my_timeline$id_str
+
+# Delete a single tweet using the post_destroy parameter
+post_destroy(tweet_ids[1])
+
+# Do a little loop to delete all tweets
+for (i in seq_along(tweet_ids)) {
+  message("Deleting Tweet ", i, " of ", length(tweet_ids))
+  post_destroy(tweet_ids = tweet_ids[i])
+}
