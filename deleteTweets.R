9a09dfcfdf7666c5d84b79af00323973ddd75e32
Scott Jacobson <65079562+scojac-github@users.noreply.github.com>
2022-11-23 21:41:20 -0500

    Add files via upload

--- deleteTweets.R
+++ deleteTweets.R
@@ -16,20 +16,15 @@ head(tweets, 5)
 # Save tweets for safekeeping
 write_csv(tweets, "data/tweets.csv")
 
-# Get your timeline
-my_timeline <- get_timeline(user)
-head(my_timeline, 5)
-colnames(my_timeline)
-
-# Create a list of all tweets
-tweet_ids <- my_timeline$id_str
+# Create a list of all tweet ids
+tweet_ids <- tweets$id_str
 
 # Convert to data frame
 tweet_df <- as.data.frame(tweet_ids)
-head(tweet_df)
 
 # Do a loop to delete the tweets
 for (i in seq_along(tweet_df$tweet_ids)) {
   message("Deleting tweet number ", i)
   post_destroy(tweet_df$tweet_ids[i])
 }
+
